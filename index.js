02014651287c1f2620e773558437354c16271a12
Jeffery Grigsby <jeffery.grigsby@yahoo.com>
2024-05-26 21:17:55 -0700

    assignment done

--- index.js
+++ index.js
@@ -4,8 +4,30 @@ const pc = newPlayableCharacter(100, 110)
 // Create a non-playable character
 const npc = newNonPlayableCharacter(50, 300)
 
-// have the NPC start walking east immediately
-npc.walkEast()
+// (SCRAPPED) have the NPC start walking east immediately 
+// npc.walkEast()
+
+// our async function to move the npc around
+async function moveNPC(){
+    await npc.walkNorth(1400)
+    await npc.walkEast(1200)
+    await npc.walkSouth(300)
+    await npc.walkEast(1500)
+    await npc.walkSouth(1500)
+    await npc.walkWest(2700)
+    await npc.walkNorth(400)
+    console.log("repeating")
+    move(npc.element).to(50, 300)
+    moveNPC()
+}
+moveNPC()
+
+// Totals
+// North: 1800
+// South: 1800
+
+// East: 2700
+// West: 2700
 
 // Create the inventory
 const inventory = newInventory()
